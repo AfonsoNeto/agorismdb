2f126e014f358c3a0daec22a964d06a824a3fa91
Afonso Neto <afonso.pontesneto@gmail.com>
2018-12-06 22:07:12 -0300

    Showing categories and giving credit where is due (Manual do Agorista)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     @resources  = Resource.eager_load(:categories).order(random)
-    @categories = Category.joins(:resources).order(random)
+    @categories = Category.joins(:resources).order(random).uniq
   end
 
   private
