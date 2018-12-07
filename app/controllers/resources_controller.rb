1e6e32aedcb5eb93b595a1801aa0cf712083af10
Afonso Neto <afonso.pontesneto@gmail.com>
2018-12-06 22:18:14 -0300

    Creating Resources controller and moving root applciation url to it's index

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,4 +1,4 @@
-class HomeController < ApplicationController
+class ResourcesController < ApplicationController
   def index
     @resources  = Resource.eager_load(:categories).order(random)
     @categories = Category.joins(:resources).order(random).uniq
