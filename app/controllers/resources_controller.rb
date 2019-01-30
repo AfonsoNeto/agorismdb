935fe2257b2d87eba89d5f7faff066f2487fa2d2
Afonso Neto <afonso.pontesneto@gmail.com>
2019-01-29 21:53:21 -0300

    Implementing search from text input and turning some code into partials for it to be replicable

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -5,6 +5,15 @@ class ResourcesController < ApplicationController
     @resources  = Resource.eager_load(:categories).order(random)
   end
 
+  def search
+    @resources = Resource.joins(:categories).where(%{
+      resources.name         ILIKE :query OR
+      resources.description  ILIKE :query OR
+      categories.name        ILIKE :query OR
+      categories.description ILIKE :query
+    }, query: "%#{params[:search_resource_query]}%").distinct
+  end
+
   def show
     @resource = Resource.find_by(id: params[:id]).try(:decorate)
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: 'resources#index'
-  resources :resources, only: [:index, :show]
+
+  resources :resources, only: [:index, :show] do
+    get :search, on: :collection, constraints: {format: :json}
+  end
 end
