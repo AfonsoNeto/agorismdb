85b16bdfda79ab0e8666cb53488526241d088c2c
Afonso Neto <afonso.pontesneto@gmail.com>
2019-01-30 22:22:29 -0300

    Showing query and button to clear search

--- app/helpers/resources_helper.rb
+++ app/helpers/resources_helper.rb
@@ -1,2 +1,6 @@
 module ResourcesHelper
+  def clear_search_button
+    url = search_resources_path(search_resource_query: nil)
+    link_to "Limpar busca", url, class: 'btn btn-default btn-sm mb-5', remote: true
+  end
 end
