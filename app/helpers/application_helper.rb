468c13eb1412718f8c14f2a127d92bba4c3883fb
Afonso Neto <afonso.pontesneto@gmail.com>
2019-02-01 23:19:34 -0300

    Random related phrases to the search input placeholder

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def random_search_phrase
+    [
+      'O mercado negro é a economia que existe contra os interesses do estado e em favor dos interesses da população.',
+      'O que busca para driblar a regulação governamental hoje?',
+      'As instituições que melhor servem o bem comum emergiram sem a intervenção de uma vontade comum e deliberada para criá-los.',
+      'Buscar recurso...'
+    ].sample
+  end
 end
