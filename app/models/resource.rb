414e30a89b0cb04f988ce68a2475017c842d8133
Afonso Neto <afonso.pontesneto@gmail.com>
2019-02-05 22:32:36 -0300

    Implementing #as_indexed_json for resource model, including categories on it

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -5,4 +5,13 @@ class Resource < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   has_many :category_resource
   has_many :categories, through: :category_resource
+
+  def as_indexed_json(options={})
+    self.as_json(
+      only: [:name, :description, :url],
+      include: {
+        categories: { only: :name }
+      }
+    )
+  end
 end
