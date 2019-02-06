d871ce283101bcea386dcb66b18585229d1a5a7a
Afonso Neto <afonso.pontesneto@gmail.com>
2019-02-05 22:32:02 -0300

    Including elasticseearch-model concerns to Resource model

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,4 +1,7 @@
 class Resource < ApplicationRecord
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+
   validates :name, presence: true, uniqueness: true
   has_many :category_resource
   has_many :categories, through: :category_resource
