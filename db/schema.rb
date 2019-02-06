388c403a5eb652a1bb0464bfa69f8abc24df48ac
Afonso Neto <afonso.pontesneto@gmail.com>
2019-02-05 22:20:23 -0300

    Removing useless column 'description' from categories table

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_06_003806) do
+ActiveRecord::Schema.define(version: 2019_02_06_011853) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string "name", null: false
-    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
