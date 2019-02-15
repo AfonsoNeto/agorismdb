0e3f529f298070d6b86cc20a844dd38c7299e893
Afonso Neto <afonso.pontesneto@gmail.com>
2019-02-14 21:20:52 -0300

    Starting testing validations for category model

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    it { expect(build(:category)).to be_valid }
+
+    context 'invalid when' do
+      let(:category) { build :category }
+
+      context 'name' do
+        context 'is not present' do
+          before { category.name = nil }
+          it { expect(category).to be_invalid }
+        end
+
+        context 'already taken' do
+          let(:name) { Faker::Lorem.word }
+          before do
+            create :category, name: name
+            category.name = name
+          end
+
+          it { expect(category).to be_invalid }
+        end
+      end
+    end
+  end
 end
