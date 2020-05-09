68d9843781c64c2065ceb4a2402fd33709f23aff
Afonso Neto <aneto@arbnco.com>
2020-05-09 23:01:44 +0100

    [close #2] [Models] Considering categories names on searching for Resources

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,8 @@
 class Category < ApplicationRecord
+  include PgSearch::Model
+
+  multisearchable against: :name
+
   validates :name, presence: true, uniqueness: true
   has_many :category_resource
   has_many :resources, through: :category_resource
