f858261ce444b6c1e24dae6e23d1a8873f9337eb
Afonso Neto <aneto@arbnco.com>
2020-05-09 22:35:41 +0100

    [#2] [DB] Creating pg_search_documents for using multisearch

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_02_171433) do
+ActiveRecord::Schema.define(version: 2020_05_09_212900) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,15 @@ ActiveRecord::Schema.define(version: 2020_05_02_171433) do
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_category_resources_on_category_id"
     t.index ["resource_id"], name: "index_category_resources_on_resource_id"
+  end
+
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text "content"
+    t.string "searchable_type"
+    t.bigint "searchable_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id"
   end
 
   create_table "resources", force: :cascade do |t|
